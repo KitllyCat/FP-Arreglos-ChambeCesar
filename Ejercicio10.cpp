07c33eee16e3ee3ef01148c1919577e090ebd5f5
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-22 18:44:22 -0500

    Ejercicio 10, moda,media y mediana de 30 numeros aleatorios (faltan las funciones principales)

---
+++ Ejercicio10.cpp
@@ -0,0 +1,37 @@
+//Generar 30 números aleatorios entre 100 y 1000 guardarlos en un arreglo y
+//calcular estadísticas de tendencia central media, mediana y moda.
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+void randNum(){
+	
+}
+
+void moda(){
+	
+}
+
+void media(){
+	
+}
+
+void prom(){
+	
+}
+int main(){
+	srand(time(0));
+	SetConsoleOutputCP(CP_UTF8);
+	const int tam=30;
+	int nums[tam];
+
+	cout<<"El programa generara 30 numeros random's, y luego procedera a mostrar la moda, media y mediana..."<<endl;
+	cout<<endl;
+	system("pause");
+	system("cls");
+
+	
+	return 0;
+}
