cea58f2b21a91fc30fb442cdb57571999edc02df
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-23 00:13:45 -0500

    Version final, ejercicio 13, convertir str2 en str1(se añadio la funcion)

--- Ejercicio13.cpp
+++ Ejercicio13.cpp
@@ -5,21 +5,26 @@ using namespace std;
 
 //La función strcpy sirve para copiar cadenas de caracteres y ubicarlas en otra variable
 
+void mistrcpy(char str1[], const char str2[]){
+    int i=0;
+    for(i=0;str2[i]!='\0';i++){
+		str1[i]=str2[i];
+    }
+
+    str1[i]='\0';
+}
+
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
 	char str1[100],str2[100];
-    int i;
+
     cout<<"Ingrese str1: ";
     cin>>str1;
     cout<<"Ingrese str2: ";
     cin>>str2;
     cout<<endl;
     
-	for(i=0;str2[i]!='\0';i++){
-		str1[i]=str2[i];
-    }
-
-    str1[i]='\0';
+    mistrcpy(str1,str2);
     
     cout<<"Ahora str1 se convirtio en str2 (copia)"<<endl<<endl;
     cout<<"str1 ahora es "<<str1<<endl;
