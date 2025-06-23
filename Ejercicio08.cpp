f13fde90e7cb7e094b883c87cff7ba54e592306d
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-22 23:11:34 -0500

    Version final, ejercicio 08, verificar si un numero es capicúa o no(se arreglo un bug de logica)

--- Ejercicio08.cpp
+++ Ejercicio08.cpp
@@ -6,7 +6,7 @@ int main(){
 	SetConsoleOutputCP(CP_UTF8);
 	const int tam = 10; //tamaño max del numero
 	long long numero10; //tamaño mas amplio
-	int nums[10], i=0, capicua=0;
+	int nums[10], i=0;
 	
 	cout<<"Ingrese un número de 10 cifras para comprobar si es capicuo: ";
 	cin>>numero10;
@@ -27,20 +27,19 @@ int main(){
 		nums[i]=inT%10;
 		inT/=10;	
 	}
-
-	for(int i=0;i<(tam/2);i++){
-		if (nums[i]!=nums[tam-1-i]){
-			capicua=1;
-			break;
-		}else{
-			capicua=2;
-			break;
-		}
+	
+	bool esCapicua = true;
+	
+	for (int i=0;i<tam/2;i++){
+    	if (nums[i]!=nums[tam-1-i]){
+    	    esCapicua=false;
+    	    break;
+    	}
 	}
 
-	if(capicua=1){
+	if(esCapicua){
 		cout<<"El numero '"<<numero10<<"' si es capicua..."<<endl;
-	}else if(capicua=2){
+	}else{
 		cout<<"El numero '"<<numero10<<"' no es capicua..."<<endl;
 	}
 	
